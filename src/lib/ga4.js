dea1d4f6758708ab1d7fa292b28a7536fdd1ebf7
Emmanuel A <emmanueladekplovi1@gmail.com>
2023-11-20 04:14:26 +0100

    Update ga4.js

--- src/lib/ga4.js
+++ src/lib/ga4.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
-const GA_MEASUREMENT_ID = 'G-7PPWKH5DX1'
-const GA_API_SECRET = '7AIoL8dbSI-e0wxvGdZP9A'
+const GA_MEASUREMENT_ID = 'G-CMTYNTC9MJ'
+const GA_API_SECRET = '1iduHGjTSnSi6Sjsg6Sa6g'
 const GA_API_ENDPOINT = 'https://www.google-analytics.com/mp/collect'
 
 const ga4Track = async (eventName, eventParams) => {
-  const clientId = 'wppb-tracker-2023'
+  const clientId = 'wprb-tracker-2023'
   const url = `${GA_API_ENDPOINT}?measurement_id=${GA_MEASUREMENT_ID}&api_secret=${GA_API_SECRET}`
 
   const eventPayload = {
